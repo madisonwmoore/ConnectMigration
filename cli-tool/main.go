e9be2c95520d37d108afa4f489f3e91da28e0722
Madison <madisonwmoore@gmail.com>
2025-07-10 08:30:24 -0500

    provider builder

--- cli-tool/main.go
+++ cli-tool/main.go
@@ -1,23 +1,32 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/connect"
 	"github.com/hashicorp/hcl/v2/hclwrite"
 	"github.com/zclconf/go-cty/cty"
 )
 
 func main() {
+
+	//getRoutingProfiles("53ffe28d-757b-4ca0-99d8-4babf0e1fe0f")
+
+	createProviderFile("us-west-2")
+
 	f := hclwrite.NewEmptyFile()
-	tfFile, err := os.Create("contact_flow.tf")
+	tfFile, err := os.Create("routing_profiles.tf")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	rootBody := f.Body()
-	addRoutingProfile("agents", rootBody, RoutingProfileOptions("agents", "agents description"))
-	// addRoutingProfile("supervisor", rootBody)
+	routingProfileBody := f.Body()
+	addRoutingProfile("agents", routingProfileBody, RoutingProfileOptionsStruct{name: "agents", description: "agents description"})
+	region := "us-east-1"
+	addRoutingProfile("agents", routingProfileBody, RoutingProfileOptionsStruct{name: "agents", description: "agents description", region: &region})
 
 	// for i := 0; i < 10; i++ {
 	// 	addRoutingProfile(strconv.Itoa(i), rootBody)
@@ -27,10 +36,37 @@ func main() {
 
 }
 
+type media_concurrencies struct {
+	channel     string
+	concurrency int
+}
+
 type RoutingProfileOptionsStruct struct {
-	name        string
-	description string
-	region      string
+	name          string
+	description   string
+	region        *string
+	media_channel *[]media_concurrencies
+}
+
+func createProviderFile(region string) *hclwrite.Body {
+	f := hclwrite.NewEmptyFile()
+	tfFile, err := os.Create("providers.tf")
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	providersBody := f.Body()
+	terraformBlock := providersBody.AppendNewBlock("terraform", nil)
+	requiredProvidersBlock := terraformBlock.Body().AppendNewBlock(" required_providers", nil)
+	requiredProvidersBlock.Body().SetAttributeValue("aws", cty.ObjectVal(map[string]cty.Value{
+		"source":  cty.StringVal("hashicorp/aws"),
+		"version": cty.StringVal("~> 6.0"),
+	}))
+
+	providerBlock := providersBody.AppendNewBlock("provider", []string{"aws"})
+	providerBlock.Body().SetAttributeValue("region", cty.StringVal(region))
+	tfFile.Write(f.Bytes())
+	return providersBody
 }
 
 func RoutingProfileOptions(name string, description string) RoutingProfileOptionsStruct {
@@ -40,12 +76,35 @@ func RoutingProfileOptions(name string, description string) RoutingProfileOption
 	return rpo
 }
 
-func addRoutingProfile(name string, rootBody *hclwrite.Body, config *RoutingProfileOptionsStruct) {
+func getRoutingProfiles(instance_id string) {
+	// ctx := context.Background()
+	sdkConfig, err := config.LoadDefaultConfig(context.TODO(), config.WithSharedConfigProfile("collectors-staging"))
+	if err != nil {
+		fmt.Println("Couldn't load default configuration. Have you set up your AWS account?")
+		fmt.Println(err)
+		return
+	}
+	client := connect.NewFromConfig(sdkConfig)
+
+	InstanceId := instance_id
+	result, err := client.ListRoutingProfiles(context.TODO(), &connect.ListRoutingProfilesInput{InstanceId: &InstanceId})
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, profile := range result.RoutingProfileSummaryList {
+		fmt.Printf("  - Name: %s, ID: %s\n", *profile.Name, *profile.Id)
+	}
+
+}
+
+func addRoutingProfile(name string, rootBody *hclwrite.Body, config RoutingProfileOptionsStruct) {
 	rp := rootBody.AppendNewBlock("resource", []string{"aws_connect_routing_profile", name})
 	rp.Body().SetAttributeValue("name", cty.StringVal("w"))
 	rp.Body().SetAttributeValue("description", cty.StringVal("w"))
 	if config.region != nil {
-		rp.Body().SetAttributeValue("region", cty.StringVal("w"))
+		rp.Body().SetAttributeValue("region", cty.StringVal(*config.region))
 	}
 
 	rp.Body().SetAttributeValue("default_outbound_queue_id", cty.StringVal("world"))
---
+++ cli-tool/routing_profiles.tf
@@ -0,0 +1,23 @@
+resource "aws_connect_routing_profile" "agents" {
+  name                      = "w"
+  description               = "w"
+  default_outbound_queue_id = "world"
+  instance_id               = "world"
+  media_concurrencies {
+    channel     = "VOICE"
+    concurrency = 1
+  }
+}
+
+resource "aws_connect_routing_profile" "agents" {
+  name                      = "w"
+  description               = "w"
+  region                    = "us-east-1"
+  default_outbound_queue_id = "world"
+  instance_id               = "world"
+  media_concurrencies {
+    channel     = "VOICE"
+    concurrency = 1
+  }
+}
+
